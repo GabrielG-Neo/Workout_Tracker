78e23a4a3b7b5fbb7bbc389c236857f153635c75
NeoGabe <gabrielg28@hotmail.com>
2021-03-17 22:21:07 -0500

    routes

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose");
 const express = require("express");
-const path = require("path");
-const router = require('express').Router();
 
 const app = express();
 
@@ -9,26 +7,15 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(require("./routes/api.js"));
 
 app.use(express.static("public"));
+app.use(require("./routes/api.js"));
+app.use(require("./routes/view.js"))
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://heizi:blackberry@back2basic.tqpzh.mongodb.net/myFirstDatabase?retryWrites=true&w=majorityt", {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
-
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname + "../public/index.html"));
-  });
-
-app.get("/exercise", (req, res) => {
-    res.sendFile(path.join(__dirname + "../public/exercise.html"));
-  });
-
-app.get("/stats", (req, res) => {
-    res.sendFile(path.join(__dirname + "../public/stats.html"));
-  });
 
 
 app.listen(PORT, () => {
